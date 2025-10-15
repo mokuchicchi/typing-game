d1d1a99dea1c87d281286eb55e303e0c33939bec
MOKUDAI Shoki <mokudai.shoki@gmail.com>
2025-10-15 17:09:46 +0900

    no message

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	outputFileTracingRoot: __dirname, // このディレクトリをルートとして扱う
 };
 
 export default nextConfig;
